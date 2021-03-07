c5227d079c7e1cbe3ab2ef9fc17e4dc015dbabc3
Shakil Rafi <19642235+srafi1@users.noreply.github.com>
2021-03-07 17:15:50 -0500

    showcase page done

--- frontend/src/routes/CommunityPage.tsx
+++ frontend/src/routes/CommunityPage.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 import BasePage from '../components/BasePage';
 
+import linus from '../assets/linus.jpeg';
+import hoodie from '../assets/hoodie.jpeg';
+import squidward from '../assets/squidward.png';
+import squidwardShirt from '../assets/squidward-shirt.jpeg';
+import Showcase from '../components/Showcase';
+
 function CommunityPage() {
+  const posts = [
+    {
+      avatar: linus,
+      username: 'whoisJoe321',
+      body: 'Ay yoo guys check out my latest hoodie design!',
+      clothing: hoodie,
+    },
+    {
+
+      avatar: squidward,
+      username: 'kingsquidward',
+      body: 'Let me know which design you guys like this design, and I’ll create more in different colors!',
+      clothing: squidwardShirt,
+    }
+  ];
   return (
     <BasePage>
-      <Typography>src/routes/CommunityPage.tsx</Typography>
+      <Box m={2} textAlign="center">
+        <Typography variant="h2">Community</Typography>
+      </Box>
+      <Box display="flex" flexDirection="column" alignItems="center">
+        {posts.map(post => <Showcase {...post} />)}
+      </Box>
     </BasePage>
   );
 }
