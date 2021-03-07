e9c12b2773bc6dd21f18a35000f07e44948d77de
Shakil Rafi <19642235+srafi1@users.noreply.github.com>
2021-03-07 18:14:24 -0500

    new releases page done

---
+++ frontend/src/components/Listing.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Box, createStyles, Grid, makeStyles, Typography } from '@material-ui/core';
+import Carousel from 'react-material-ui-carousel';
+
+const useStyles = makeStyles(() => createStyles({
+  subtitle: {
+    fontFamily: 'Cabin'
+  }
+}));
+
+function Listing(props: { images: string[], name: string, price: number }) {
+  const classes = useStyles();
+  return (
+    <Box width={220}><Grid container>
+      <Grid item xs={12}>
+        <Carousel
+          autoPlay={false}
+          timeout={250}
+          navButtonsAlwaysInvisible
+          indicatorContainerProps={{
+            className: "",
+            style: {
+              marginTop: "-20px"
+            }
+          }}
+          indicatorIconButtonProps={{
+            className: "",
+            style: {
+              backgroundColor: "white",
+              margin: 1,
+            }
+          }}>
+          {props.images.map(image =>
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignContent="center"
+              width={220}
+              height={330}
+              overflow="hidden">
+              <img height="auto" src={image} />
+            </Box>
+          )}
+        </Carousel>
+      </Grid>
+      <Grid container justify="space-between">
+        <Grid item xs={9}>
+          <Typography variant="body1" className={classes.subtitle}>{props.name}</Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <Typography variant="body1" className={classes.subtitle} color="textSecondary">${props.price}</Typography>
+        </Grid>
+      </Grid>
+    </Grid></Box>
+  );
+}
+export default Listing;
--- frontend/src/routes/NewReleasesPage.tsx
+++ frontend/src/routes/NewReleasesPage.tsx
@@ -1,11 +1,82 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Box } from '@material-ui/core';
+
 import BasePage from '../components/BasePage';
+import { useTextStyles } from '../util/styles';
+import Heading from '../components/Heading';
+import Listing from '../components/Listing';
+
+import release1 from '../assets/release1.jpeg';
+import release2 from '../assets/release2.jpeg';
+import release3 from '../assets/release3.jpeg';
+import release4 from '../assets/release4.jpeg';
+import release5 from '../assets/release5.jpeg';
+import release6 from '../assets/release6.jpeg';
+import release7 from '../assets/release7.jpeg';
+import release8 from '../assets/release8.jpeg';
+import release9 from '../assets/release9.jpeg';
 
 function NewReleasesPage() {
+  const textClasses = useTextStyles();
+  const listings = [
+    {
+      images: [release1, release1, release1],
+      name: "Brown Overcoat Fit",
+      price: 450
+    },
+    {
+      images: [release2, release2, release2],
+      name: "White & Tan Semi-formal Fit",
+      price: 350
+    },
+    {
+      images: [release3, release3, release3],
+      name: "Casual Sports Fit",
+      price: 200
+    },
+    {
+      images: [release4, release4, release4],
+      name: "1850’s Victorian Fit",
+      price: 450
+    },
+    {
+      images: [release5, release5, release5],
+      name: "Classic Denim Overwear Fit",
+      price: 250
+    },
+    {
+      images: [release6, release6, release6],
+      name: "Buttermilk Trenchcoat Fit",
+      price: 400
+    },
+    {
+      images: [release7, release7, release7],
+      name: "Bold Red Hoodie Fit",
+      price: 250
+    },
+    {
+      images: [release8, release8, release8],
+      name: "Casual Sports Fit 2",
+      price: 250
+    },
+    {
+      images: [release9, release9, release9],
+      name: "Street Freewear Fit",
+      price: 200
+    },
+  ];
   return (
     <BasePage>
-      <Typography>src/routes/NewReleasesPage.tsx</Typography>
+      <Heading page="New Releases" />
+      <Box
+        width={850}
+        m="auto"
+        p={4}
+        display="flex"
+        justifyContent="space-around"
+        flexWrap="wrap">
+        {listings.map(listing => <Listing {...listing} />)}
+      </Box>
     </BasePage>
   );
 }
