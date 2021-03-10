35249bd2fd4dd04026d8eff5f5f57c953efd8b5c
Shakil Rafi <19642235+srafi1@users.noreply.github.com>
2021-03-10 17:24:11 -0500

    add preview tab

--- frontend/src/routes/CreatePage.tsx
+++ frontend/src/routes/CreatePage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Box, Tab, Tabs } from '@material-ui/core';
-import Designer from 'react-designer';
+import Designer, { Preview } from 'react-designer';
 
 import BasePage from '../components/BasePage';
 import Heading from '../components/Heading';
@@ -25,19 +25,32 @@ const TabPanel = ({ currentTab, tab, children }) => {
 function CreatePage() {
   const width = 400, height = 600;
   const [frontObjects, setFrontObjects] = useState([]);
+  const frontSvg = {
+    type: 'image',
+    x: 0, y: 0,
+    width: width, height: height,
+    xlinkHref: shirtFront
+  };
+  const backSvg = {
+    type: 'image',
+    x: 0, y: 0,
+    width: width, height: height,
+    xlinkHref: shirtBack
+  };
   const [backObjects, setBackObjects] = useState([]);
   const [tab, setTab] = useState(0);
   return (
     <BasePage>
       <Heading page="Create" />
       <Box display="flex" justifyContent="center">
-        <Box m={4} width={width + 290} border="1px solid black">
+        <Box m={4} width={2 * width} border="1px solid black">
           <Tabs
             value={tab}
             onChange={(_, t) => setTab(t)}
             variant="fullWidth">
             <Tab label="Front" />
             <Tab label="Back" />
+            <Tab label="Preview" />
           </Tabs>
           <TabPanel currentTab={tab} tab={0}>
             <ShirtBG width={width} height={height} />
@@ -52,6 +65,16 @@ function CreatePage() {
               background="transparent"
               objects={backObjects}
               onUpdate={objects => setBackObjects(objects)} />
+          </TabPanel>
+          <TabPanel currentTab={tab} tab={2}>
+            <Box width={width} display="inline-block">
+              <Preview width={width} height={height}
+                objects={[frontSvg, ...frontObjects]} />
+            </Box>
+            <Box width={width} display="inline-block">
+              <Preview width={width} height={height}
+                objects={[backSvg, ...backObjects]} />
+            </Box>
           </TabPanel>
         </Box>
       </Box>
