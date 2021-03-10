a2997ad752deb45093e849983374e1671ba8219a
Shakil Rafi <19642235+srafi1@users.noreply.github.com>
2021-03-10 15:56:27 -0500

    start create page

--- frontend/src/routes/CreatePage.tsx
+++ frontend/src/routes/CreatePage.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import { Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Box } from '@material-ui/core';
+import Designer from 'react-designer';
+
 import BasePage from '../components/BasePage';
+import Heading from '../components/Heading';
+
+import shirtFront from '../assets/shirt-front.png';
+import shirtBack from '../assets/shirt-back.png';
+
+const ShirtBG = ({ width, height, back = false }) => (
+  <svg width={width} height={height} style={{position: "absolute"}}>
+    <image x={0} y={0} width={width} height={height}
+      xlinkHref={back ? shirtBack : shirtFront} />
+  </svg>
+);
 
 function CreatePage() {
+  const width = 400, height = 600;
+  const [objects, setObjects] = useState([]);
   return (
     <BasePage>
-      <Typography>src/routes/CreatePage.tsx</Typography>
+      <Heading page="Create" />
+      <Box display="flex" justifyContent="center">
+        <Box p={2} m={4} border="1px solid black">
+          <ShirtBG width={width} height={height} />
+          <Designer width={width} height={height}
+            background="transparent"
+            objects={objects}
+            onUpdate={objects => setObjects(objects)} />
+        </Box>
+      </Box>
     </BasePage>
   );
 }
-export default CreatePage;
+export default CreatePage
